28e1ef321a419709a2c7730c2c6097d5cd7ea56d
camilo <cgs.camilogonzalez@gmail.com>
2015-01-17 16:00:17 -0500

    added more methods in the elevator class. Getter methods for limit switches

--- src/com/nutrons/recyclerush/subsystems/Elevator.java
+++ src/com/nutrons/recyclerush/subsystems/Elevator.java
@@ -1,5 +1,7 @@
 package com.nutrons.recyclerush.subsystems;
 
+import lib.DebouncedBoolean;
+
 import com.nutrons.recyclerush.RobotMap;
 
 import edu.wpi.first.wpilibj.DigitalInput;
@@ -16,11 +18,40 @@ public class Elevator extends Subsystem {
     private DigitalInput isDown = new DigitalInput(RobotMap.ELEVATOR_DOWN);
     private DigitalInput isUp = new DigitalInput(RobotMap.ELEVATOR_UP);
 	
+    private DebouncedBoolean elevatorDown = new DebouncedBoolean(5);
+    private DebouncedBoolean elevatorUp = new DebouncedBoolean(5);
+    
+    private static double driveUpPower = 0.5;
+    private static double driveDownPower = -0.5;
+    
     public void initDefaultCommand() {
     	
     }
     
+    public boolean isElevatorDown() {
+    	elevatorDown.feed(isDown.get());
+    	return elevatorDown.get();
+    }
     
+    public boolean isElevatorUp() {
+    	elevatorUp.feed(isUp.get());
+    	return elevatorUp.get();
+    }
+    
+    public void driveElevatorUp() {
+    	elevatorMotor1.set(driveUpPower);
+    	elevatorMotor2.set(driveUpPower);
+    }
+    
+    public void driveElevatorDown() {
+    	elevatorMotor1.set(driveDownPower);
+    	elevatorMotor2.set(driveDownPower);
+    }
+    
+    public void stopElevator() {
+    	elevatorMotor1.set(0);
+    	elevatorMotor2.set(0);
+    }
     
 }
 

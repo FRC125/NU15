e3f13630ae56b60a1af6511d59bee56786ea7e68
camilo <cgs.camilogonzalez@gmail.com>
2015-01-25 15:10:00 -0500

    Cleaned up some of the files. Added javadoc to OI class

--- src/com/nutrons/recyclerush/OI.java
+++ src/com/nutrons/recyclerush/OI.java
@@ -20,30 +20,58 @@ public class OI {
 	private Button fieldCentricButton = new JoystickButton(driverPad, 2);
 	private Button resetGyroButton = new JoystickButton(driverPad, 7);
 	
+	/**
+	 * Gets dPad button
+	 * @return direction of dpad
+	 */
 	public int getPOVDirection() {
 		return driverPad.getPOV();
 	}
-	
+
+	/**
+	 * Gets value on the x axis joystick (throtle)
+	 * @return x axis value
+	 */
 	public double getJoystickX() {
 		return Utils.deadband(-driverPad.getRawAxis(0), 0.1, 0);
 	}
 	
+	/**
+	 * Gets value on the y axis joystick (side to side)
+	 * @return y axis value
+	 */
 	public double getJoystickY() {
 		return Utils.deadband(driverPad.getRawAxis(1), 0.1, 0);
 	}
 	
+	/**
+	 * gets value of x axis joystick (rotate)
+	 * @return x axis value
+	 */
 	public double getJoystickSpin() {
 		return Utils.deadband(driverPad.getRawAxis(4), 0.05, 0);
 	}
 	
+	/**
+	 * Gets state on button for heading
+	 * @return is heading pressed
+	 */
 	public boolean isHoldHeading() {
 		return headingButton.get();
 	}
 	
+	/**
+	 * Gets state of fieldcentric button 
+	 * @return is filedCentric pressed
+	 */
 	public boolean isFieldCentric() {
 		return fieldCentricButton.get();
 	}
 	
+	/**
+	 * Gets state of button for zeroing the gyro
+	 * @return is resetGyro pressed
+	 */
 	public boolean isResetGyroButton() {
 		return resetGyroButton.get();
 	}

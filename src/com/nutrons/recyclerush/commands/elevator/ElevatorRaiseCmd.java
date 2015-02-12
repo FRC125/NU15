868ee14b75cf2e7b43108ab603c81a9a063d946c
John Zhang <johnzhang15@gmail.com>
2015-02-11 19:39:18 -0500

    Added elevator raise and lower commands.

---
+++ src/com/nutrons/recyclerush/commands/elevator/ElevatorRaiseCmd.java
@@ -0,0 +1,40 @@
+package com.nutrons.recyclerush.commands.elevator;
+
+import com.nutrons.recyclerush.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class ElevatorRaiseCmd extends Command {
+
+    public ElevatorRaiseCmd() {
+        requires(Robot.elevator);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	Robot.elevator.setElevatorPower(0.5);
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return Robot.elevator.isAtMaxHeight();
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    	Robot.elevator.stop();
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    	end();
+    }
+}

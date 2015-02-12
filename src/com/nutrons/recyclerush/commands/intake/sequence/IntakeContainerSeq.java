050e19a9ece07bbb893069e215690899a9246623
John Zhang <johnzhang15@gmail.com>
2015-02-12 17:14:07 -0500

    Added intake sequences.

---
+++ src/com/nutrons/recyclerush/commands/intake/sequence/IntakeContainerSeq.java
@@ -0,0 +1,17 @@
+package com.nutrons.recyclerush.commands.intake.sequence;
+
+import com.nutrons.recyclerush.commands.intake.IntakeOpenCmd;
+import com.nutrons.recyclerush.commands.intake.SpinIntakeWheelsCmd;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ * @author John Zhang
+ */
+public class IntakeContainerSeq extends CommandGroup {
+    
+    public  IntakeContainerSeq() {
+    	addSequential(new IntakeOpenCmd());
+    	addSequential(new SpinIntakeWheelsCmd());
+    }
+}

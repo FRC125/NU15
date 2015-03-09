adb82d9d72ab516036ef7f8f5f4e462e5d9137f8
John Zhang <johnzhang15@gmail.com>
2015-03-09 18:36:03 -0400

    Changed three tote autonomous to use knock tote strategy.

--- src/com/nutrons/recyclerush/commands/auto/AutoThreeTotes.java
+++ src/com/nutrons/recyclerush/commands/auto/AutoThreeTotes.java
@@ -2,13 +2,7 @@ package com.nutrons.recyclerush.commands.auto;
 
 import com.nutrons.recyclerush.commands.elevator.ElevatorLowerCmd;
 import com.nutrons.recyclerush.commands.elevator.ElevatorRaiseCmd;
-import com.nutrons.recyclerush.commands.elevator.LowerElevatorIfStackableCmd;
-import com.nutrons.recyclerush.commands.intake.IntakeCloseCmd;
-import com.nutrons.recyclerush.commands.intake.IntakeOpenCmd;
-import com.nutrons.recyclerush.commands.intake.SpinIntakeWheelsCmd;
-import com.nutrons.recyclerush.commands.intake.StopIntakeWheelsCmd;
-import com.nutrons.recyclerush.commands.intake.WaitForStackableCmd;
-import com.nutrons.recyclerush.commands.intake.WaitForToteCmd;
+import com.nutrons.recyclerush.commands.intake.sequence.SpitIntakeSeq;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import edu.wpi.first.wpilibj.command.WaitCommand;
@@ -19,31 +13,34 @@ import edu.wpi.first.wpilibj.command.WaitCommand;
 public class AutoThreeTotes extends CommandGroup {
     
     public  AutoThreeTotes() {
-    	addSequential(new IntakeOpenCmd());
-    	addSequential(new AutoDriveUntilToteCmd()); // first tote
-    	addSequential(new WaitForToteCmd());
-    	addSequential(new IntakeCloseCmd());
-    	addSequential(new SpinIntakeWheelsCmd());
-    	addSequential(new WaitForStackableCmd());
-    	addSequential(new LowerElevatorIfStackableCmd());
-    	addSequential(new ElevatorRaiseCmd());
-    	addSequential(new IntakeOpenCmd());
-    	addSequential(new WaitForToteCmd());
-    	addSequential(new IntakeCloseCmd());
-    	addSequential(new SpinIntakeWheelsCmd());
-    	addSequential(new WaitForStackableCmd());
-    	addSequential(new LowerElevatorIfStackableCmd());
-    	addSequential(new ElevatorRaiseCmd());
-    	addSequential(new IntakeOpenCmd());
-    	addSequential(new WaitForToteCmd());
-    	addSequential(new IntakeCloseCmd());
-    	addSequential(new SpinIntakeWheelsCmd());
-    	addSequential(new WaitForStackableCmd());
-    	addSequential(new AutoStopDriveCmd());
     	addSequential(new ElevatorLowerCmd());
-    	addSequential(new StopIntakeWheelsCmd());
-    	addSequential(new AutoTurnAngleCmd(90)); // Turn right 90 degrees
-    	addSequential(new AutoDriveDistanceCmd(10)); // Must figure this distance
-    	addSequential(new AutoDriveDistanceCmd(-5));
+    	addSequential(new ElevatorRaiseCmd());
+    	addSequential(new WaitCommand(1));
+    	addSequential(new AutoDriveDistanceCmd(30));
+    	addSequential(new WaitCommand(1));
+    	addSequential(new AutoTurnAngleCmd(90));
+    	addSequential(new SpitIntakeSeq());
+    	addSequential(new AutoTurnAngleCmd(-90));
+    	addSequential(new AutoDriveUntilToteCmd());
+    	
+    	addSequential(new ElevatorLowerCmd());
+    	addSequential(new ElevatorRaiseCmd());
+    	addSequential(new WaitCommand(1));
+    	addSequential(new AutoDriveDistanceCmd(30));
+    	addSequential(new WaitCommand(1));
+    	addSequential(new AutoTurnAngleCmd(90));
+    	addSequential(new SpitIntakeSeq());
+    	addSequential(new AutoTurnAngleCmd(-90));
+    	
+    	addSequential(new ElevatorLowerCmd());
+    	addSequential(new ElevatorRaiseCmd());
+    	addSequential(new WaitCommand(1));
+    	addSequential(new AutoDriveDistanceCmd(30));
+    	addSequential(new WaitCommand(1));
+    	addSequential(new AutoTurnAngleCmd(90));
+    	addSequential(new SpitIntakeSeq());
+    	
+    	addSequential(new AutoDriveDistanceCmd(150));
+    	addSequential(new ElevatorLowerCmd());
     }
 }

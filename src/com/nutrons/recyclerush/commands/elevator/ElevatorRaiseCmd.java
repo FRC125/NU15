884c93593aaa39cc8a174a6d27c7e41391c37d4d
camilo <cgs.camilogonzalez@gmail.com>
2015-03-13 09:00:04 -0400

    Fixed intake elevator lowering logic.

--- src/com/nutrons/recyclerush/commands/elevator/ElevatorRaiseCmd.java
+++ src/com/nutrons/recyclerush/commands/elevator/ElevatorRaiseCmd.java
@@ -17,6 +17,7 @@ public class ElevatorRaiseCmd extends Command {
     // Called just before this Command runs the first time
     protected void initialize() {
     	Robot.intake.retractHolderPistons();
+    	Robot.intake.openIntakeWheel();
     }
 
     // Called repeatedly when this Command is scheduled to run
